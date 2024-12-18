f7dfcc1771d0488728555c6c11fcca471bcc2243
Bobby Wang <bobbyquennell@gmail.com>
2024-12-18 20:48:45 +1100

    Improve Naming Conventions

--- infra/serviceAStack.ts
+++ infra/serviceAStack.ts
@@ -10,17 +10,17 @@ export class ServiceAStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const worker = new NodejsFunction(this, `${stackName}-lambda-${appEnv}`, {
+    const serviceALambdaFunction = new NodejsFunction(this, `${stackName}-lambda-${appEnv}`, {
       functionName: `${stackName}-lambda-${appEnv}`,
-      description: 'a lambda worker of example service A',
+      description: `a lambda function of stack ${stackName} in ${appEnv} env`,
       runtime: lambda.Runtime.NODEJS_18_X,
       architecture: lambda.Architecture.ARM_64,
       entry: path.join(__dirname, '../src/serviceA/handler.ts'),
       handler: 'handler',
       logRetention: RetentionDays.THREE_MONTHS,
     });
-    new cdk.CfnOutput(this, 'worker', {
-      value: worker.functionArn,
+    new cdk.CfnOutput(this, `${stackName}-lambda-${appEnv}`, {
+      value: serviceALambdaFunction.functionArn,
       description: `the arn of the lambda function: ${stackName}-lambda-${appEnv}`,
       exportName: `${stackName}-lambda-${appEnv}`,
     });
